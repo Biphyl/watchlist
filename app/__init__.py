ae3a4a718396704dc24b5b7ed3254598305af104
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-24 13:27:22 +0300

    modify

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from app import views
+from app import error
 
 #Initializing application
-app = Flask(__name__,instance_relative_config=True)
+app = Flask(__name__,instance_relative_config=True,)
 
-#Setting up configuration
+#Setting up configurationa
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
+# Initializing Flask Extensions
+Bootstrap = Bootstrap(app)
 
 from app import views
