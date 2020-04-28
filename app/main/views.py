e24702e2a719a3d681c13248c0d2e8789fd9bd99
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-28 05:02:03 +0300

    modify structure

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
+from .models import ReviewForm
+Review = review.Review
 
 
 # Views
@@ -39,6 +41,7 @@ def movie(id):
     '''
     movie = get_movie(id)
     title = f'(movie.title)'
+    reviews = Review.get_reviews(movie.id)
 
     return render_template('movie.html',title = title, movie = movie, reviews = reviews)
 
@@ -52,3 +55,18 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for (movie_name)'
     return render_template('search.html',movies = searched_movies)
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
---
+++ start.sh
@@ -0,0 +1,2 @@
+ export MOVIE_API_KEY = '67a2ba1b9154d3439f072828a03dcf5f'
+export SECRET_KEY = 'lovine'
