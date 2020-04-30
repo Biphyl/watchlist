8b2f0dedce7b3213bd80da567e2fd96d0b1228da
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-30 14:15:12 +0300

    created a file upload form

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,9 @@ def create_app(config_name):
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
+    # configure UploadSet
+    configure_uploads(app,photos)
+
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
