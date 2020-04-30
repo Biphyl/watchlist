63ad9efca10c92ec89794bd164c75f2c93e6e6c1
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-30 17:04:42 +0300

    install flask-mail

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
- export MOVIE_API_KEY = '67a2ba1b9154d3439f072828a03dcf5f'
+export MOVIE_API_KEY = '67a2ba1b9154d3439f072828a03dcf5f'
 export SECRET_KEY = 'lovine'
+
+export MAIL_USERNAME=<Your Email Address>
+        # 'bironodhiambo00@gmail.com'
+export MAIL_PASSWORD=<Your Email password>
 
 python3 manage.py server
