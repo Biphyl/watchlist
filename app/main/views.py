fcbd48560e6f636bdddcc8cdf44f23b953b815ba
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-01 04:28:42 +0300

    updated reviews model

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from ..requests import get_movies,get_movie,search_movie
 from .forms import ReviewForm,UpdateProfile
 from .. import db,photos
 from ..models import Review,User
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 
 # Views
@@ -67,18 +67,18 @@ def search(movie_name):
 @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 @login_required
 def new_review(id):
-
     form = ReviewForm()
-
     movie = get_movie(id)
 
     if form.validate_on_submit():
         title = form.title.data
         review = form.review.data
 
-        new_review = review(movie.id,title,movie.poster,review)
-        new_review.save_review()
+        # Updated review instance
+        new_review = Review(movie_id=movie.id,movie_title=title,image_path=movie.poster,movie_review=review,user=current_user)
 
+        # save review method
+        new_review.save_review()
         return redirect(url_for('.movie',id = movie.id ))
 
     title = f'{movie.title} review'
